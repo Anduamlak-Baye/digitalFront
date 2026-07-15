826c26a78ebd5693fb2d2232b796801143c4add7
Anduamlak <andutok808@gmail.com>
2026-07-15 19:53:21 +0300

    Azz

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,9 @@ function App(){
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<h1>Fuck you nigga</h1>}/>
-      <Route path='/:restaurantId' element={<Items/>}/>
+      {/* <Route path='/:restaurantId' element={<Items/>}/> */}
+      <Route path='/:restaurantId' element={<h1>Second Page</h1>}/>
+      
       <Route path='/item-detail/:resId/:id' element={<ItemsDetail/>}/>
       <Route path='/admin-panel' element = {<AdminPanel/>}/>
       <Route path='/login' element={<Login/>}/>
